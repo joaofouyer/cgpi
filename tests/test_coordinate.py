fa5275fc5c5e65a793b3a815682850a8fec89968
joaofouyer <jfouyer@gmail.com>
2019-08-17 01:15:18 -0300

    using xvfb to test GUI

--- tests/test_coordinate.py
+++ tests/test_coordinate.py
@@ -1,7 +1,7 @@
 from src.primitives.coordinate import Coordinate
 
 
-def test_main():
+def test_coordinate():
     coordinate = Coordinate(x=50, y=50)
     assert type(coordinate) == Coordinate
     assert coordinate.x == 50
