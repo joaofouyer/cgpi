933722487f46dab1d1bec1e5e8e846631489899f
joaofouyer <jfouyer@gmail.com>
2019-08-17 01:11:06 -0300

    changed tests because it's not possible to test tkinter in travis

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
-from src.window import Window
+from src.primitives.coordinate import Coordinate
 
 
 def test_main():
-    w = Window(title="Testando Pontos Animados", width=640, height=480, background="#000000")
-    assert type(w) == Window
+    coordinate = Coordinate(x=50, y=50)
+    assert type(coordinate) == Coordinate
+    assert coordinate.x == 50
+    assert coordinate.y == 50
+    assert coordinate.get_coordinate() == (50, 50)
