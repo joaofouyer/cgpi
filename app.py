a0d4e445ad700af037afe87dfc5df74539501be7
Caroline Cristina de Oliveira Appolinario <ra00172420@pucsp.edu.br>
2019-08-29 19:18:28 -0300

    quase la

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ from gui.window import Window
 from primitives.coordinate import Coordinate
 from primitives.point import Point
 from primitives.line_graph import LineGraph
+from primitives.circle import Circle
+from primitives.circle_graph import CircleGraph
 
 
 class App:
+    
     @staticmethod
-    def main():
+    def menhe():
         try:
             w = Window(title="Testando Pontos Animados", width=640, height=480, background="white")
-            coordinate_p1 = Coordinate(x=50, y=50)
+            coordinate_p1 = Coordinate(x=50, y=500)
             p1 = Point(window=w, coordinate=coordinate_p1)
-            coordinate_p2 = Coordinate(x=200, y=50)
-            p2 = Point(window=w, coordinate=coordinate_p2)
-            line = LineGraph(p1=p1, p2=p2, color="blue", thickness=2)
-            line.draw(w=w, animation=False)
-
+            cc = CircleGraph(p1, 250, "#000000", 2)
+            cc.drawCircle()
             w.mainloop()
             return False
 
         except Exception as e:
             print("Exception on main(): ", e)
             return True
-
-
+    
+    
 
 app = App()
-app.main()
+app.menhe()
--- primitives/circle_graph.py
+++ primitives/circle_graph.py
@@ -1,1 +1,38 @@
 # coding: utf-8
+from primitives.circle import Circle
+from primitives.point import Point
+
+class CircleGraph (Circle, object):
+    def _init_(self, center, radius, color,  thickness):
+        if sys.version_info[0] < 3:
+            super(CircleGraph, self).__init__(center, radius)
+        else:
+            super().__init__(center, radius)   
+        
+        self.color = color
+        self.thickness = thickness
+
+    def set_properties(self, window, point):
+        try:
+            p = point
+            p.color = self.color
+            p.size = self.thickness
+            p.window = window
+            p.draw()
+        except Exception as e:
+            print("Exception on set_properties: ", e)
+            return True   
+    def drawCircle(self, window):
+        try:
+            for angle in range(0,360):
+                xc = cc.build_circle_x(angle)
+                yc = cc.build_circle_y(angle)
+                coordinate_cc = Coordinate(x=xc, y=yc)
+                pt = point.Point(window, coordinate_cc, size=self.thickness, color=self.color)
+                pt.draw()
+        except Exception as e:
+            print("hiriririr", e)
+            return True
+
+
+        
