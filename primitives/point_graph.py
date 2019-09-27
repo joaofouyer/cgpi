50545b4a18dab5d6e5b864b4710695896b89b224
fouyer <jfouyer@gmail.com>
2019-09-27 18:35:10 -0300

    working viewport with clicks

--- primitives/point_graph.py
+++ primitives/point_graph.py
@@ -18,6 +18,13 @@ class PointGraph (Point, object):
             if append_action:
                 self.window.actions.push(self=self)
             self.window.canvas.create_oval(self.x-self.size, self.y-self.size, self.x + self.size, self.y + self.size, fill=self.color, outline=self.color)
+
+            vp_x, vp_y = self.window.viewport.reduce(x=self.x, y=self.y, window=self.window)
+            self.window.viewport.canvas.create_oval(
+                vp_x - 1, vp_y - 1, vp_x + 1, vp_y + 1, fill=self.color,
+                outline=self.color
+            )
+
             if window:
                 window.refresh()
             return False
