4e80df45522b8ee10ac60a8b3e4d8726cdcb6af5
Joao Estenio Lima Fouyer <ra00187107@pucsp.edu.br>
2019-09-27 19:03:47 -0300

    clean canvas on app.py

--- app.py
+++ app.py
@@ -9,13 +9,7 @@ from structures.action import Action
 
 def main():
     try:
-        w = Window(title="Testando Desfazer Ações", width=700, height=650, background="white", actions=Action())
-        rec = RectangleGraph(p1=Point(x=25, y=50), p2=Point(x=250, y=200), color="#000000", thickness=2)
-        rec.draw(window=w)
-        cc = CircleGraph(center=Point(x=120, y=120), radius=50, color="#000000", thickness=2)
-        cc.draw(window=w)
-        line = LineGraph(p1=Point(x=50, y=120), p2=Point(x=500, y=120))
-        line.draw(window=w)
+        w = Window(title="Janela com Viewport", width=1500, height=800, background="white", actions=Action())
         w.mainloop()
         return False
 
